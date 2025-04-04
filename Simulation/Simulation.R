b909442df6cc3a8eda142ec39f31bfc1627896aa
YaofangHuYaofang <149336655+YaofangHuYaofang@users.noreply.github.com>
2025-04-04 10:07:31 -0500

    Simulation.R

--- Simulation/Simulation.R
+++ Simulation/Simulation.R
@@ -4,37 +4,47 @@ library(pracma)
 source("Various Functions.R")
 source("Mtable.R")
 
+
 ############ Parameter Set  ############
+
 
 # Simulate the Network
 #n = 1000; # number of nodes
 #K1 = 4; # number of communities revealed by adjacency matrix
 
 
+
+
 # Set the labels
 set.seed(2019)
 l = sample(1:K1, n, replace=TRUE); # node labels
+
 
 Pi = matrix(0, n, K1) # label matrix
 for (k in 1:K1){
   Pi[l == k, k] = 1
 }
 
+
 # use theta from "ExpSets.R" and the generated labels for the dense community and sparse communities
-theta[l < 3] = theta[l < 3]*0.3 + 0.3; #dense community
-theta[l >= 3] = theta[l >= 3]*0.03 + 0.03; #sparse communities
+theta[l < 3] = theta[l < 3]*0.2 + 0.3; #dense community
+theta[l >= 3] = theta[l >= 3]*0.02 + 0.03; #sparse communities
 Theta = diag(theta); # node degree heterogeneity
+
 
 # Get the expected adjacency matrix
 Omega = Theta %*% Pi %*% P %*% t(Pi) %*% Theta;
+
 
 ############ End Parameter Set  ############
 errormat = matrix(0, nrow = repetition, ncol = 12) #store the error rate in each repitition
 colnames(errormat) = c("New", "New2", "CASC", "ADMM", "Net_based", "Cov_based", "New,small", "New2,small",
                        "CASC,small", "ADMM,small", "Cov_based, small", "giant/all")
 
+
 #Calculation of the error 
 bigp = 600; smallp = 20;
+
 
 for (ii in 1: repetition){
   set.seed(ii+9999)
@@ -46,10 +56,11 @@ for (ii in 1: repetition){
 #    mu = 0.2;
   Qsmall = 0.1*matrix(sign(runif(smallp*K2) - 0.5), nrow = smallp); 
   for(smalli in 1:K2){
-    Qsmall[(smalli-1)*(smallp/K2)+(1:(smallp/K2)), smalli] = mu + 0.1; #remark. has a change here
+    Qsmall[(smalli-1)*(smallp/K2)+(1:(smallp/K2)), smalli] = mu + 0.3; #remark. has a change here
   }
-#    signal_big = rbinom(bigp, 1, 2*sqrt(bigp)/bigp);
-  signal_big = rbinom(bigp, 1, 0.1);
+
+
+  signal_big = rbinom(bigp, 1, 0.05);#parameter changed
   Qbig = mu*matrix(rep(signal_big,K2),nrow=bigp,byrow=FALSE)*matrix(sign(runif(bigp*K2) - 0.5), nrow = bigp);
     W = matrix(0, nrow = n, ncol = K2); 
     
@@ -75,6 +86,7 @@ for (ii in 1: repetition){
       Dbig[i,] = rnorm(bigp, mean = Dbig0[,i], sd = 1);
   }
 
+
   A = matrix(runif(n*n, 0, 1), nrow = n);
   A = Omega - A;
   A = 1*(A >= 0)
@@ -88,15 +100,18 @@ for (ii in 1: repetition){
   errormat[ii, "giant/all"] = length(giantLabel)/n;
 
 
+
+
   ######### This is the section for large p ###################
-  # New approach: NAC
+  # New approach: CA-SCORE
   degree = colSums(A); alpha = mean(degree)/2;
   est_nac = NAC(A, Dbig, K1, alpha = alpha, beta = 0)
   comp = table(est_nac, l);
   err_nac = cluster(comp)$error;
   errormat[ii, "New"] = err_nac;
 
-  # New approach: NAC-generalized
+
+  # New approach: CA-SCORE
   degree = colSums(A); alpha = mean(degree)/2;
   est_nagc = NAC(A, Dbig, K1, alpha = alpha, beta = 1)
   comp = table(est_nagc, l);
@@ -104,10 +119,11 @@ for (ii in 1: repetition){
   errormat[ii, "New2"] = err_nagc;
   
   # CA-clustering
-  est_casc = CAclustering(A, Dbig, K1)
+  est_casc = CAclustering(A, Dbig, K1,  alphan = 10)
   comp = table(est_casc, l);
   err_casc = cluster(comp)$error;
   errormat[ii, "CASC"] = err_casc;
+
 
   # admm
   est_admm = admm(A, C = Dbig, K = K1, lambda = 0.5, alpha = 2, rho = 0.5, TT = 100, tol = 100)
@@ -115,7 +131,7 @@ for (ii in 1: repetition){
   errormat[ii, "ADMM"] = cluster(comp)$error;
   
   # Network-based method RSC
-  est_net <- Net_based(Adj = A, K = K1)
+  est_net <- Net_based(Adj = A, K = K1, tau = log(n))
   comp = table(est_net, l);
   errormat[ii, "Net_based"] = cluster(comp)$error;
   
@@ -124,15 +140,16 @@ for (ii in 1: repetition){
   comp = table(est_cov, l);
   errormat[ii, "Cov_based"] = cluster(comp)$error;
 
+
   ######### This is the section for small p ###################
-  # New approach: NAC
+  # New approach: CA-SCORE
   degree = colSums(A); alpha = mean(degree)/2;
   est_nac = NAC(A, Dsmall, K1, alpha = alpha, beta = 0)
   comp = table(est_nac, l);
   err_nac = cluster(comp)$error;
   errormat[ii, "New,small"] = err_nac;
   
-  # New approach: NAC-generalized
+  # New approach: CA-SCORE
   degree = colSums(A); alpha = mean(degree)/2;
   est_nagc = NAC(A, Dsmall, K1, alpha = alpha, beta = 1)
   comp = table(est_nagc, l);
@@ -140,10 +157,11 @@ for (ii in 1: repetition){
   errormat[ii, "New2,small"] = err_nagc;
   
   # CA-clustering
-  est_casc = CAclustering(A, Dsmall, K1)
+  est_casc = CAclustering(A, Dsmall, K1,  alphan = 10)
   comp = table(est_casc, l);
   err_casc = cluster(comp)$error;
   errormat[ii, "CASC,small"] = err_casc;
+
 
   # admm
   est_admm = admm(A, C = Dsmall, K = K1, lambda = 0.5, alpha = 2, rho = 0.5, TT = 100, tol = 100)
